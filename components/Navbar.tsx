3a62bcc2b8ac5a1362ae76eab13fb1879ddd0b63
Abdulrahman-Elwan <137184416+Abdulrahman-Elwan@users.noreply.github.com>
2026-09-09 20:32:33 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ const links = [
   { href: "#home", label: "الرئيسية" },
   { href: "#services", label: "خدماتنا" },
   { href: "#gallery", label: "أعمالنا" },
-  { href: "#sectors", label: "المناطق" },
+  { href: "#why-us", label: "لماذا نحن" },
   { href: "#testimonials", label: "آراء عملائنا" },
   { href: "#contact", label: "تواصل معنا" },
 ];
